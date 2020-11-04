74bda11909ae2d46e167ecc6cd5e384a49ba4b00
Susi Lehtola <susi.lehtola@gmail.com>
2020-11-04 13:05:42 -0500

    Added the quadruple-zeta x2c basis sets.

---
+++ x2c-qzvp/add-template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from basis_set_exchange import curate
+
+# Two components?
+for twoc in [False, True]:
+    # Number of polarization shells
+    for npol in range(1,3):
+        # For NMR shieldings?
+        for nmr in [False, True]:
+            # Basis set name is
+            basname='x2c-QZVPall' if npol==1 else 'x2c-QZVPPall'
+            # Description
+            description='All-electron relativistic polarized quadruple-zeta basis' if npol==1 else 'All-electron relativistic doubly polarized quadruple-zeta basis'
+            if twoc:
+                basname+='-2c'
+                description+=' for one-component calculations'
+            else:
+                description+=' for two-component calculations'
+
+            if nmr:
+                basname+='-s'
+                description+=' of NMR shielding'
+
+            curate.add_basis(bs_file='{}.tm.bz2'.format(basname),
+                             data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
+                             subdir='ahlrichs_x2c',
+                             file_base=basname,
+                             name=basname,
+                             family='ahlrichs_x2c',
+                             role='orbital',
+                             description=description,
+                             version='1',
+                             revision_description='Data from Supporting info',
+                             data_source='Supporting info of franzke2020a',
+                             refs='franzke2020a',
+                             file_fmt=None
+)
+
+curate.add_basis(bs_file='x2c-jbas.tm.bz2',
+                 data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
+                 subdir='ahlrichs_x2c',
+                 file_base='x2c-jbas',
+                 name='x2c-jbas',
+                 family='ahlrichs_fit',
+                 role='jfit',
+                 description='Coulomb fitting auxiliary basis for x2c basis sets',
+                 version='1',
+                 revision_description='Data from Supporting info',
+                 data_source='Supporting info of franzke2019a',
+                 refs='franzke2020a',
+                 file_fmt=None
+)
