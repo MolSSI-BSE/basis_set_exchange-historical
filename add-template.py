5c16bc1b2083c97535db35633a29d728818ab1a4
Benjamin Pritchard <ben@bennyp.org>
2020-11-04 10:40:29 -0500

    Add template script

---
+++ add-template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from basis_set_exchange import curate
+
+curate.add_basis(bs_file='',
+                 data_dir='/home/ben/programming/bse_ng/basis_set_exchange/basis_set_exchange/data',
+                 subdir='',
+                 file_base='',
+                 name='',
+                 family='',
+                 role='',
+                 description='',
+                 version='',
+                 revision_description='',
+                 data_source='',
+                 refs='',
+                 file_fmt=None
+)
