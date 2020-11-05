6c96f5c5d9c45e2f923a1747af2944bab6da8d1d
Susi Lehtola <susi.lehtola@gmail.com>
2020-11-05 15:28:28 -0500

    Had to concatenate the pob-TZVP files for easier addition.

--- pob/add-template.py
+++ pob/add-template.py
@@ -3,21 +3,6 @@
 from basis_set_exchange import curate
 
 if False:
-    curate.add_basis(bs_file='pob-TZVP.crystal.bz2',
-                     data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
-                     subdir='pob',
-                     file_base='pob-TZVP',
-                     name='pob-TZVP',
-                     family='pob',
-                     role='orbital',
-                     description='pob-TZVP',
-                     version=1,
-                     revision_description='Data from Bredow\'s web site',
-                     data_source='Data from Bredow\'s web site',
-                     refs='Peintinger2013a',
-                     file_fmt=None
-                     )
-
     for basis in ['pob-DZVP-rev2', 'pob-TZVP-rev2']:
         print('Adding {}'.format(basis))
         curate.add_basis(bs_file='{}.crystal.bz2'.format(basis),
@@ -34,3 +19,18 @@ if False:
                          refs='Oliveira2019b',
                          file_fmt=None
                         )
+
+curate.add_basis(bs_file='pob-TZVP.crystal.bz2',
+                 data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
+                 subdir='pob',
+                 file_base='pob-TZVP',
+                 name='pob-TZVP',
+                 family='pob',
+                 role='orbital',
+                 description='pob-TZVP',
+                 version=1,
+                 revision_description='Data from Bredow\'s web site',
+                 data_source='Data from Bredow\'s web site',
+                 refs={'H,Li-F,Na-Cl,K-Br' : 'Peintinger2013a', 'Rb-Mo,Ru-I' : 'Laun2018a'},
+                 file_fmt=None
+                 )
