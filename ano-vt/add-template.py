238e226e7995e8f5960172eadbf5ee1ad2f8336b
Susi Lehtola <susi.lehtola@gmail.com>
2020-12-16 11:03:17 -0500

    ANO-VT-QZ cleanup

--- ano-vt/add-template.py
+++ ano-vt/add-template.py
@@ -2,33 +2,17 @@
 
 from basis_set_exchange import curate
 
-for basis in ['ANO-VT-DZ', 'ANO-VT-TZ','ANO-VT-QZ']:  
-    curate.add_basis(bs_file='{}.txt.bz2'.format(basis),
+curate.add_basis(bs_file='ANO-VT-QZ.2.genbas.bz2',
                  data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
                  subdir='ano_claudino',
-                 file_base=basis,
-                 name=basis,
+                 file_base='ANO-VT-QZ',
+                 name='ANO-VT-QZ',
                  family='ano_claudino',
                  role='orbital',
-                 description=basis,
+                 description='ANO-VT-QZ (Atomic Natural Orbital - Virial Theorem - Quadruple Zeta)',
                  version=2,
-                 revision_description='Data from github/danclaudino (ACES format); Gaussian\'94 format on the site is broken',
+                 revision_description='Retranslated from ACES format; original Gaussian\'94 data had negative exponents',
                  data_source='Data from D. Claudino',
-                 refs={'claudino2016a', 'claudino2016b'},
-                 file_fmt='genbas'
-                 )
-for basis in ['FANO-DZ', 'FANO-TZ','FANO-QZ', 'FANO-5Z', 'FANO-6Z']:  
-    curate.add_basis(bs_file='{}.txt.bz2'.format(basis),
-                 data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
-                 subdir='ano_claudino',
-                 file_base=basis,
-                 name=basis,
-                 family='ano_claudino',
-                 role='orbital',
-                 description=basis,
-                 version=2,
-                 revision_description='Data from github/danclaudino (ACES format); Gaussian\'94 format on the site is broken',
-                 data_source='Data from D. Claudino',
-                 refs={'claudino2018a'},
+                 refs=['claudino2016a', 'claudino2016b'],
                  file_fmt='genbas'
                  )
