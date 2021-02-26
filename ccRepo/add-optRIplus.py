a798e941a379bf72fb7facb1eef346fef9744272
Susi Lehtola <susi.lehtola@gmail.com>
2021-02-25 21:39:45 -0500

    Updated optRI+ addition script

--- ccRepo/add-optRIplus.py
+++ ccRepo/add-optRIplus.py
@@ -3,14 +3,15 @@
 from basis_set_exchange import curate
 
 for b in ['cc-pVDZ-F12_OPTRIplus', 'cc-pVTZ-F12_OPTRIplus', 'cc-pVQZ-F12_OPTRIplus']:
+    name=b.replace('_','-').replace('plus','+')
     curate.add_basis(bs_file='parsed/{}.gbasis'.format(b),
                      data_dir='/home/work/basis_set_exchange/basis_set_exchange/data',
                      subdir='dunning_fit',
-                     file_base=b,
-                     name=b.replace('plus','+'),
+                     file_base=name,
+                     name=name,
                      family='dunning_fit',
                      role='optri',
-                     description=b.replace('plus','+'),
+                     description=name,
                      version=1,
                      revision_description="Data from Grant Hill's ccRepo",
                      data_source="Data from Grant Hill's ccRepo",
