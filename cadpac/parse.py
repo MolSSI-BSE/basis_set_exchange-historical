1015a7eb54cf38b9aee6e852c3f039022632fb79
Susi Lehtola <susi.lehtola@gmail.com>
2022-03-17 14:00:28 -0400

    Forgot to commit final CADPAC files

--- cadpac/parse.py
+++ cadpac/parse.py
@@ -13,6 +13,9 @@ note_re = re.compile(r'^NOTE ')
 # Terminator
 terminator_re = re.compile(r'^\s*\*\*\*\*\*\s*$')
 
+# Shell enetry
+shell_re = re.compile(r'^\s*(\d+)\s+([A-Za-z]+)\s+(\d+)\s*$')
+
 # Collect the basis sets by name in a dictionary
 basis_sets = {}
 element_sections = partition_lines(data, element_re.match)
@@ -29,20 +32,53 @@ for iel, el in enumerate(element_sections):
     # Check termination
     if el[-1].strip() != '*****':
         raise RuntimeError('Incorrect terminator: {}'.format(el[-1].strip()))
+    # Remove last line
+    el.pop()
 
     # Check that there is no extra terminator
     nterm = sum([1 if terminator_re.match(line) else 0 for line in el])
-    if nterm != 1:
+    if nterm != 0:
         for iline, line in enumerate(el):
             if terminator_re.match(line):
                 raise RuntimeError('Problem in entry: {}'.format(el[iline+1]))
-    
+
     # Extract the name of the basis set
     elname, Z = parse_line_regex(element_re, el[0], 'elementbasisname, Z')
-    
+
     # Clean out the basis set name from the string
     sym = element_sym_from_Z(Z).upper()
     assert elname[:len(sym)] == sym
-    basname = elname[len(sym):]
+    basname = elname[len(sym):].strip()
 
     print('basis = {} sym = {}'.format(basname, sym))
+
+    # Parse the shells
+    shell_sections = partition_lines(el[1:], shell_re.match)
+
+    # Basis set in Gaussian'94 format
+    elstr = '{} 0\n'.format(element_sym_from_Z(Z, normalize=True))
+    for shell in shell_sections:
+        # First line is just shell definition
+        elstr += ' '.join(shell[0].split()[1:]) + ' 1.00\n'
+        for line in shell[1:]:
+            elstr += '    ' + '    '.join(line.split()[1:]) + '\n'
+    elstr += '****\n'
+
+    if basname not in basis_sets:
+        basis_sets[basname] = []
+    basis_sets[basname].append((Z, elstr))
+
+# Sort the basis sets by element
+for basname in basis_sets:
+    basis_sets[basname] = sorted(basis_sets[basname], key=lambda x: x[0])
+
+# Write out the basis sets
+for basname in basis_sets:
+    # Sort the basis sets by
+
+    print('writing {}'.format(basname))
+    fout=open('{}.gbs'.format(basname), 'w')
+
+    basis = [x[1] for x in basis_sets[basname]]
+    fout.write(''.join(basis))
+    fout.close()
